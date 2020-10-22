27fc50b67e1b2abd5e0f5936fab8230d83908cec
Marko Karg <mkarg@redhat.com>
2020-10-22 16:57:42 +0200

    initial commit

---
+++ kafka/08-grafana-datasource.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# create Prometheus datasource
+curl -X POST http://admin:admin@grafana-route-amq.apps.test736.myocp4.com/api/datasources -d @grafana-dashboards/datasource.json --header "Content-Type: application/json"
+# deploy Kafka dashboard
+curl -X POST http://admin:admin@grafana-route-amq.apps.test736.myocp4.com/api/dashboards/db -d @grafana-dashboards/strimzi-kafka.json --header "Content-Type: application/json"
+# deploy Zookeeper dashboard
+curl -X POST http://admin:admin@grafana-route-amq.apps.test736.myocp4.com/api/dashboards/db -d @grafana-dashboards/strimzi-zookeeper.json --header "Content-Type: application/json"
+
+## Note : Manually importing the Ceph dashboard works. This needs to be fixed
+
+# deploy Ceph dashboard
+#curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/ceph-cluster-dashboard.json --header "Content-Type: application/json"
+# deploy Ceph dashboard
+#curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/ceph-osds-dashboard.json --header "Content-Type: application/json"
+# deploy Ceph dashboard
+#curl -X POST http://admin:admin@grafana-route-amq.apps.ocp4.ceph-s3.com/api/dashboards/db -d @grafana-dashboards/ceph-pools-dashboard.json --header "Content-Type: application/json"
+~                                                                                                                                                                            
+~                                                                                                                                                                            
+~                                          
